c637b42647a470c1db6514075f1914dbeea59380
Rajat Goyal <404rajat@gmail.com>
2015-03-14 18:39:48 +0530

    all templates rendering fine

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-angular.module('discussionApp', ['postController', 'postService']);
+// angular.module('discussionApp', ['postController', 'postService']);
+
+// var app=angular.module('discussionApp',['postController', 'postService']).
+// config(['$routeProvider', function($routeProvider){
+//           $routeProvider.
+//                 when("/", {templateUrl: "views/feed.html", controller: "postController"}).
+//                 when("/signin", {templateUrl: "views/signin.html", controller: "postController"}).
+//                 when("/feed", {templateUrl: "views/feed.html", controller: "postController"}).
+//                 otherwise({redirectTo: '/'});
+//             }]);
+
+
+
+angular.module('discussionApp',['ngRoute', 'postController', 'postService']).
+config(['$routeProvider', '$locationProvider', function($routeProvider) {
+          $routeProvider.
+                when("/", {templateUrl: "views/feed.html", controller: "mainController"}).
+                when("/signin", {templateUrl: "views/signin.html", controller: "mainController"}).
+                when("/feed", {templateUrl: "views/feed.html", controller: "mainController"}).
+                otherwise({redirectTo: '/'
+            });
+}]);
+
