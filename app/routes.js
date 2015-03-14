291d0912601e8e9e9443acca5612ec814ac51e8d
Rajat Goyal <404rajat@gmail.com>
2015-03-15 04:26:50 +0530

    changes

--- app/routes.js
+++ app/routes.js
@@ -1,44 +1,9 @@
 var Comment = require('./models/posts');
-// var Account = require('./models/accounts');
-// var passport = require('passport');
 
-// function getPosts(res){
-// 	Comment.find(function(err, posts) {
-
-// 			if (err)
-// 				res.send(err)
-
-// 			res.json(posts); 
-// 		});
-// };
-
-// module.exports = function(app) {
-	
-// 	app.get('/api/posts', function(req, res) {
-// 		getPosts(res);
-// 	});
-	
-// 	app.post('/api/posts', function(req, res) {
-
-// 		Comment.create({
-// 			text : req.body.text,
-// 			// userId = req.user._id,
-// 			done : false
-// 		}, function(err, posts) {
-// 			if (err)
-// 				res.send(err);
-		
-// 			getPosts(res);
-// 		});
-
-// 	});
-
-
-// };
 exports.getPosts = function(req, res){
 	Comment.find(function(err, posts) {
 			if (err)
-				res.send(err)
+				res.send(err);
 
 			res.json(posts); 
 		});
@@ -54,30 +19,7 @@ exports.addPosts = function(req, res){
 			if (err)
 				res.send(err);
 		
-			getPosts(res);
+			exports.getPosts(res);
 		});
 	};
 
-// module.exports = function(app) {
-	
-// 	app.get('/api/posts', function(req, res) {
-// 		getPosts(res);
-// 	});
-	
-// 	app.post('/api/posts', function(req, res) {
-
-// 		Comment.create({
-// 			text : req.body.text,
-// 			// userId = req.user._id,
-// 			done : false
-// 		}, function(err, posts) {
-// 			if (err)
-// 				res.send(err);
-		
-// 			getPosts(res);
-// 		});
-
-// 	});
-
-
-// };
